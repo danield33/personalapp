7bb2da7524040c9d07a4c70cbaf418e219b253bd
Daniel <danieldawit33@gmail.com>
2021-08-21 22:11:29 -0400

    Add Items

--- src/App.js
+++ src/App.js
@@ -1,16 +1,45 @@
-import logo from './logo.svg';
 import './App.css';
 
 import React, {Component} from 'react';
 
 class App extends Component {
-  render() {
-    return (
-        <div className="App">
-          
-        </div>
-    );
-  }
+
+    state = {
+        items: []
+    }
+    currInputItem = "";
+    inputRef;
+
+
+    handleSubmit = (event) => {
+        this.state.items.push(this.currInputItem);
+        this.setState({items: this.state.items})
+        this.currInputItem = "";
+        this.inputRef.value = "";
+        event.preventDefault();
+    }
+
+    handleItemChange = (event) => {
+        this.currInputItem = event.target.value;
+    }
+
+    render() {
+        return (
+            <div className="App">
+
+                    {
+                        this.state.items.map((item, index) => <p key={index}>{item}</p>)
+                    }
+
+                <form className={"flexRow"} onSubmit={this.handleSubmit}>
+                    <p style={{padding: 20}}>Input an Item</p>
+                    <input ref={(ref) => this.inputRef = ref}
+                           placeholder={"Type something"}
+                           onChange={this.handleItemChange} />
+                </form>
+            </div>
+        );
+    }
 }
 
 export default App;
